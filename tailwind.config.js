cc91873cf59ab1282c2aeeff560b1a4f0941008e
amanuel2000 <amanuelf44@gmail.com>
2023-11-18 14:55:14 +0300

    updated images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ['./*.html'],
   theme: {
     extend: {
-      screens:{
-        "sm":"480px"
+      spacing:{
+        "big":"48rem"
       }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
     },
     fontFamily:{
       nunito:['Nunito', 'sans-serif']
@@ -13,4 +19,3 @@ module.exports = {
   },
   plugins: [],
 }
-
